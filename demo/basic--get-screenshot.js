10ee0cace0bbf3bdf8ae13328b364a296a2dd7fa
Eric L. Hepperle <codewizard13@gmail.com>
2022-12-31 12:11:43 -0600

    UPDATE DEMO:  Get basic screenshot

--- demo/basic--get-screenshot.js
+++ demo/basic--get-screenshot.js
@@ -12,18 +12,13 @@ File Version:    1.00.00
 /* _____________ ALGORITHM __________________ */
 
 const Puppeteer = require('puppeteer')
-const fs = require('fs/promises') // we don't have to write messy callback code
+
 async function start() {
 
   const browser = await Puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('https://learnwebcode.github.io/practice-requests/')
-  // await page.screenshot({path: "amazing.png"})
-
-  const names = ['red', 'green', 'blue']
-  await fs.writeFile('names.txt', names.join('\n'))
-
-  
+  await page.screenshot({path: "amazing2.png"})
 
   await browser.close()
 }
