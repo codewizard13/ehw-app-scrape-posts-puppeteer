b1d5981edc1c1a57ac8575468f1bc57b4c115739
Eric L. Hepperle <codewizard13@gmail.com>
2023-01-08 08:18:15 -0600

    ADD algorithm.txt:  MOVE algo to own file

--- app5.js
+++ app5.js
@@ -1,12 +1,10 @@
 /*
-Project Name:   EHW APP: App: Scrape Blog Posts with Puppet
+Project Name:   EHW APP:  Scrape Local HTML File
 Date Created:   12/31/22
+Date Modified:  01/08/22
 Programmer:     Eric L. Hepperle
 
-TESTING grabbing url list from 
-https://www.elijahlist.com/words/index.html?page=3
-
-File Version:    5
+PARSE LOCAL FILE FOR HTML
 
 RESOURCES:
 - https://www.youtube.com/watch?v=lgyszZhAZOI
@@ -14,47 +12,6 @@ RESOURCES:
 */
 
 
-
-/*
-
-ALGORITHM:
-
-REQUIRE puppeteer and define (Puppeteer)
-REQUIRE nodejs file system promises module
-REQUIRE dirname command from path module
-
-DEFINE url to search as one containing "display_word" and an ID number (TARGET_URL)
-
-// FUNCTION: getPageDataAsJSON() ASYNC
-DEFINE browser const as await Puppeteer.launch()
-DEFINE page const as await browser.newPage()
-AWAIT page.goto(TARGET_URL)
-
-DEFINE data object as having
-  EMPTY head object
-  EMPTY body object
-
-GET THE FOLLOWING:
-- ALL meta tags
-- ALL style tags
-- ALL script tags
-- ALL link tags
-
-- TITLE
-- AUTHOR
-- Publish date
-- Steve Desk
-- AUTHOR LOCATION
-- AUTHOR PHOTO
-- Word Content
-- Author email
-- Author Website
-- Author Ministry name
-- Author Description
-
-RETURN as object
-
-*/
 
 
 const { filenameToLines, parseCSVToArray, testUrls, CONSTANTS } = require(`${__dirname}/common/io`)
